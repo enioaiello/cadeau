b07d602086a15212ee32d7314f0b02095ef3cf1d
Maitre Gims <150128658+MaitregGims@users.noreply.github.com>
2024-11-29 15:39:52 +0100

    Add files via upload

---
+++ cadeau/assets/js/script.js
@@ -0,0 +1,29 @@
+const students = [
+    {
+        name: 'Théo',
+        link: './views/theo.html',
+    },
+    {
+        name: 'Jane Doe',
+        link: './views/jane-doe/index.html',
+    }
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const studentList = document.querySelector('#cards');
+    students.forEach(student => {
+        studentList.innerHTML += `
+            <div class="col-12 col-md-6 col-lg-4">
+    <div class="card mb-4">
+        <div class="card-body">
+            <h5 class="card-title">${student.name}</h5>
+            <a href="${student.link}" class="btn btn-primary">Voir la carte</a>
+        </div>
+    </div>
+</div>`;
+    });
+
+    const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    myModal.show();
+});
