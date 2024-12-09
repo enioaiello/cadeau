7f0d2a5b7cb06247c7e336d1c0920371c46f98a6
SIBYLLE HUEZ EREMBERT <119930225+6billGate@users.noreply.github.com>
2024-12-09 21:38:41 +0100

    Add files via upload

---
+++ views/darkSibyll/js/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  init: function() {
+    // console.log('initialisation du module theme');
+       const bodyElem = document.querySelector('body');
+      bodyElem.classList.toggle('theme-disco');
+  
+}}
+
+// Initialize the theme module
+theme.init();
